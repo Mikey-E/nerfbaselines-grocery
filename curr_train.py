ef4ca5aa617926cf07bd0517eada0e8121814fce
Michael E. Elgin <melgin@uwyo.edu>
2024-10-10 18:25:13 -0600

    curr_train updates

--- curr_train.py
+++ curr_train.py
@@ -18,13 +18,13 @@ import sys
 #base_path = "/project/3dllms/DATASETS/GROUPED/Fruits/Pears"
 
 #base_path = "/project/3dllms/DATASETS/GROUPED/"
-base_path = "/project/3dllms/DATASETS/CONVERTED/"
-base_path += sys.argv[1]
+#base_path = "/project/3dllms/DATASETS/CONVERTED/"
+#base_path += sys.argv[1]
 
 #resume = "04_04_2024_W_C_Melons2_F_1"
 #found = False
 
-for folder in os.listdir(base_path):
+#for folder in os.listdir(base_path):
 #    if folder != resume and found == False:
 #        continue
 #    else:
@@ -35,7 +35,25 @@ for folder in os.listdir(base_path):
 #        " --output results/gof_" + folder
 #    command = "nerfbaselines train --method gaussian-splatting --data " + base_path + "/" + folder + \
 #        " --output results/gs_" + folder
-    command = "nerfbaselines train --method mip-splatting --data " + base_path + "/" + folder + \
-        " --output results/ms_" + folder
-    print("Starting: " + command)
-    os.system(command)
+#    command = "nerfbaselines train --method mip-splatting --data " + base_path + "/" + folder + \
+#        " --output results/ms_" + folder
+#    print("Starting: " + command)
+#    os.system(command)
+
+#data_path = "/cluster/medbow/project/3dllms/DATASETS/PROCESSED/04_19_2024_W_F_Sugar_P_2/"
+#folder = data_path.split("/")[-2]
+#assert "_2024_" in folder, "folder does not match format expected of folders"
+#
+#command = "nerfbaselines train --method zipnerf --data " + data_path + \
+#    " --output results/poc_zn_" + folder
+#print(command)
+#os.system(command)
+
+data_path = "/cluster/medbow/project/3dllms/DATASETS/CONVERTED/UNGROUPED/04_19_2024_W_F_Sugar_P_2/"
+folder = data_path.split("/")[-2]
+assert "_2024_" in folder, "folder does not match format expected of folders"
+
+command = "nerfbaselines train --method gaussian-opacity-fields --data " + data_path + \
+    " --output results/poc_gof_" + folder
+print(command)
+os.system(command)
