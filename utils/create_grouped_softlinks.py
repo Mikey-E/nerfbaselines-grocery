fa78f12f96691a8a5eece8f2e1da60fe81b0fa31
Michael E. Elgin <melgin@uwyo.edu>
2024-10-12 01:56:55 -0600

    refactored code to create dataset soft-links

--- utils/create_grouped_softlinks.py
+++ utils/create_grouped_softlinks.py
@@ -1,31 +1,58 @@
 #Author: Michael Elgin
-#File to make softlinks for the converted dataset in its grouped folder
+#File to make softlinks for an ungrouped dataset in its corresponding grouped folder.
+#This assumes the actual data is stored in an ungrouped folder, and grouped soft-links should point to it based on
+#the original grouping hierarchy of categories and subcategories
 
 import os
+import argparse
 
-#Necessary environment variables
-GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH = os.getenv("GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH")
-GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH = os.getenv("GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH")
-GROCERY_DATA_NORMAL_GROUPED_DIR_PATH = os.getenv("GROCERY_DATA_NORMAL_GROUPED_DIR_PATH")
+parser = argparse.ArgumentParser(description="create grouped soft-links for a copy of the dataset")
+parser.add_argument(
+    "--new_ungrouped_dir_path",
+    type=str,
+    required=True,
+    help="Flat location of scene folders where the grouped soft-links will point to"
+)
+parser.add_argument(
+    "--new_grouped_dir_path",
+    type=str,
+    required=True,
+    help="Location of where to place the grouped soft-links"
+)
+parser.add_argument(
+    "--old_grouped_dir_path",
+    type=str,
+    default="/project/3dllms/DATASETS/GROUPED/",
+    help="Location of where current grouped data folders exist, to model after"
+)
+args = parser.parse_args()
 
-#Ensure they are set
-assert(GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH != None)
-assert(GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH != None)
-assert(GROCERY_DATA_NORMAL_GROUPED_DIR_PATH != None)
+#Ensure the provided paths exist
+if not os.path.exists(args.new_ungrouped_dir_path):
+    raise FileNotFoundError(f"Path '{args.new_ungrouped_dir_path}' doesn't exist. Provide a valid path.")
+if not os.path.exists(args.new_grouped_dir_path):
+    raise FileNotFoundError(f"Path '{args.new_grouped_dir_path}' doesn't exist. Provide a valid path.")
+if not os.path.exists(args.old_grouped_dir_path):
+    raise FileNotFoundError(f"Path '{args.old_grouped_dir_path}' doesn't exist. Provide a valid path.")
 
-for category in os.listdir(GROCERY_DATA_NORMAL_GROUPED_DIR_PATH):
-    os.makedirs(GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH + category, exist_ok=True)
+#paths should end in a '/' for subsequent code
+args.new_ungrouped_dir_path = args.new_ungrouped_dir_path.rstrip("/") + "/"
+args.new_grouped_dir_path = args.new_grouped_dir_path.rstrip("/") + "/"
+args.old_grouped_dir_path = args.old_grouped_dir_path.rstrip("/") + "/"
+
+for category in os.listdir(args.old_grouped_dir_path):
+    os.makedirs(args.new_grouped_dir_path + category, exist_ok=True)
     if category == "Random": #is effectively a sub-category
-        for scene_folder in os.listdir(GROCERY_DATA_NORMAL_GROUPED_DIR_PATH + category):
-            command = "ln -s " + GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH + scene_folder + " "\
-            + GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH + category + "/" + scene_folder
+        for scene_folder in os.listdir(args.old_grouped_dir_path + category):
+            command = "ln -s " + args.new_ungrouped_dir_path + scene_folder + " "\
+            + args.new_grouped_dir_path + category + "/" + scene_folder
             print(command)
             os.system(command)
         continue
-    for subcategory in os.listdir(GROCERY_DATA_NORMAL_GROUPED_DIR_PATH + category):
-        os.makedirs(GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH + category + "/" + subcategory, exist_ok=True)
-        for scene_folder in os.listdir(GROCERY_DATA_NORMAL_GROUPED_DIR_PATH + category + "/" + subcategory):
-            command = "ln -s " + GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH + scene_folder + " "\
-            + GROCERY_DATA_CONVERTED_GROUPED_DIR_PATH + category + "/" + subcategory + "/" + scene_folder
+    for subcategory in os.listdir(args.old_grouped_dir_path + category):
+        os.makedirs(args.new_grouped_dir_path + category + "/" + subcategory, exist_ok=True)
+        for scene_folder in os.listdir(args.old_grouped_dir_path + category + "/" + subcategory):
+            command = "ln -s " + args.new_ungrouped_dir_path + scene_folder + " "\
+            + args.new_grouped_dir_path + category + "/" + subcategory + "/" + scene_folder
             print(command)
             os.system(command)
