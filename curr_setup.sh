926e235b6ccd9c5444f15b430b0c8582b49d19cd
Michael E. Elgin <melgin@uwyo.edu>
2024-10-18 14:50:59 -0600

    curr_setup for new gs env

--- curr_setup.sh
+++ curr_setup.sh
@@ -2,10 +2,10 @@
 
 #Setup for 2dgs
 #conda remove --prefix /project/3dllms/melgin/conda/envs/conda-envs/2d-gaussian-splatting/6fddd194c2e0d4432a95071a67ac3f539cea8667abb76d609a0f0c2f1f7cfc24/2d-gaussian-splatting --all -y
-export CURR_SCENE="04_19_2024_W_F_Sugar_P_2/"
+#export CURR_SCENE="04_19_2024_W_F_Sugar_P_2/"
 #export CURR_SCENE="04_19_2024_W_F_Spices_P_2/"
-export CURR_DATA_DIR_PATH=$GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH
-nerfbaselines train --method 2d-gaussian-splatting --data ${CURR_DATA_DIR_PATH}${CURR_SCENE} --output ${GROCERY_RESULTS_DIR_PATH}poc_2dgs_${CURR_SCENE}
+#export CURR_DATA_DIR_PATH=$GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH
+#nerfbaselines train --method 2d-gaussian-splatting --data ${CURR_DATA_DIR_PATH}${CURR_SCENE} --output ${GROCERY_RESULTS_DIR_PATH}poc_2dgs_${CURR_SCENE}
 
 #Setup for zipnerf
 #export CURR_SCENE="04_19_2024_W_F_Sugar_P_2/"
@@ -16,3 +16,8 @@ nerfbaselines train --method 2d-gaussian-splatting --data ${CURR_DATA_DIR_PATH}$
 #export CURR_SCENE="04_19_2024_W_F_Sugar_P_2/"
 #export CURR_DATA_DIR_PATH=$GROCERY_DATA_NORMAL_UNGROUPED_DIR_PATH
 #nerfbaselines train --method instant-ngp --data ${CURR_DATA_DIR_PATH}${CURR_SCENE} --output ${GROCERY_RESULTS_DIR_PATH}poc_ingp2_${CURR_SCENE}
+
+#Setup for gaussian-splatting ablation new env
+export CURR_SCENE="04_19_2024_W_F_Sugar_P_2/"
+export CURR_DATA_DIR_PATH=$GROCERY_DATA_CONVERTED_UNGROUPED_DIR_PATH
+nerfbaselines train --method gaussian-splatting --data ${CURR_DATA_DIR_PATH}${CURR_SCENE} --output ${GROCERY_RESULTS_DIR_PATH}poc_gs_${CURR_SCENE}
