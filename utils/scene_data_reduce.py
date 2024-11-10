a8d6ce79e5956327d08f72d5f043e5a6b026b850
Michael Elgin <michaelelgin1@gmail.com>
2024-11-09 18:52:50 -0800

    scene_data_reduce creating filtered_transforms, in a state of damage fixing

--- utils/scene_data_reduce.py
+++ utils/scene_data_reduce.py
@@ -6,6 +6,7 @@
 
 import argparse
 import os
+import json
 
 parser = argparse.ArgumentParser(description="reduce data in all scene folders (for ablation studies)")
 parser.add_argument(
@@ -30,8 +31,28 @@ assert args.percent >= 0 and args.percent <= 100
 for scene_folder in os.listdir(args.path):
 	images_in_scene = os.listdir(args.path + scene_folder + "/images")
 	total_images_in_scene = len(images_in_scene)
-	threshold = (args.percent/100) * total_images_in_scene
-	images_to_remove = [image for image in images_in_scene if int(image.split(".")[0]) > threshold]
-	for images_folder in ["images", "images_2", "images_4", "images_8"]:
-		for image in images_to_remove:
-			os.system("rm " + args.path + scene_folder + "/" + images_folder + "/" + image)
+#	threshold = (args.percent/100) * total_images_in_scene
+#	images_to_remove = [image for image in images_in_scene if int(image.split(".")[0]) > threshold]
+#	for images_folder in ["images", "images_2", "images_4", "images_8"]:
+#		for image in images_to_remove:
+#			os.system("rm " + args.path + scene_folder + "/" + images_folder + "/" + image)
+
+	#Now update the transforms.json - leaving unmatched frames will cause errors,
+	#that means having a transform for an image that no longer exists will cause errors.
+	with open(args.path + scene_folder + "/transforms.json", 'r') as file:
+		data=json.load(file)
+
+	threshold=total_images_in_scene#@@@
+
+	#Filter frames based on the image number in the file path
+	filtered_frames=[
+		frame for frame in data['frames']
+		if int(frame['file_path'].split('/')[-1].split('.')[0])<=threshold
+	]
+
+	#Update the data with filtered frames
+	data['frames']=filtered_frames
+
+	#Save the filtered data back to JSON
+	with open(args.path + scene_folder + "/filtered_transforms.json", 'w') as file:
+		json.dump(data, file, indent=4)
