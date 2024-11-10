6848a7d1011126570156083bb4c9bfd01c8d8b22
Michael Elgin <michaelelgin1@gmail.com>
2024-11-10 13:11:10 -0700

    command_split.py comment update

--- multi_sbatch/command_split.py
+++ multi_sbatch/command_split.py
@@ -3,6 +3,8 @@
 #For example: if you have created a file from running something like:
 #	python multi-train.py nerfacto --data_path /home2/grocery/data/normal/100percent/ungrouped/ --results_path /home2/grocery/results/nerfbaselines-grocery/nerfacto_4/ > nerfacto_4_commands.sh
 #Then this file can take nerfacto_4_commands.sh and split it into n chunks for n gpus
+#This will then allow setting each split to its own gpu:
+#	CUDA_VISIBLE_DEVICES=[0|1|2...] nohup ./nerfacto_4_commands_split[0|1|2...].sh > /home2/grocery/logs/nerfbaselines-grocery/nerfacto_4_split[0|1|2...].log &
 
 import argparse
 import os
